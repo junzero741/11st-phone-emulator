e2f5ce557d07baa6c31b92b81b0f15fc532c5ba1
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-20 23:14:48 +0900

    fix : getDate 함수 월 표시 수정 * Date 객체의 month는 0부터 시작하므로, 1 더해주었음.

--- src/ts/utils/getDate.ts
+++ src/ts/utils/getDate.ts
@@ -2,7 +2,7 @@ const getDate = (): string => {
   let result = '';
   const date = new Date();
   const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   const hours = date.getHours();
   const minutes = date.getMinutes();
