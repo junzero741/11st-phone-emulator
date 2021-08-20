077eb39acda211d6ee667181ea9e6c357cb43103
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-20 17:52:35 +0900

    feat : 웹팩 환경설정 * 엔트리파일 : 프로젝트 루트의 index.ts * 아웃풋파일 : 프로젝트 루트의 dist/index.js * html 엔트리 및 아웃풋 : 프로젝트 루트의 index.html

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './index.ts',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'index.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // output file name
+      template: 'index.html', // template file name
+    }),
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist'],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  devtool: prod ? undefined : 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
