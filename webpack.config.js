d22701162bb00711e6b9d353669002858c40f3ff
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-22 13:03:24 +0900

    feat : 파일 로더 모듈 설치

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,14 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   devtool: prod ? undefined : 'source-map',
