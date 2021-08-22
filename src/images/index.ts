cbaf1adbb7bfd53c5ff0b92079497362f0f4eeab
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-22 13:02:15 +0900

    feat : 이미지 파일 저장 * 고양이 사진5장, 강아지 사진 5장

---
+++ src/images/index.ts
@@ -0,0 +1,16 @@
+// import * as CAT_1 from './cat_1.jpg';
+// import * as CAT_2 from './cat_2.jpg';
+// import * as CAT_3 from './cat_3.jpeg';
+// import * as CAT_4 from './cat_4.png';
+// import * as CAT_5 from './cat_5.jpg';
+// import * as DOG_1 from './dog_1.jpg';
+// import * as DOG_2 from './dog_2.jpg';
+// import * as DOG_3 from './dog_3.jpg';
+// import * as DOG_4 from './dog_4.jpg';
+// import * as DOG_5 from './dog_5.jpg';
+
+// export * as cat_1 from './cat_1.jpg';
+// export * from './cat_2.jpg';
+// export * from './cat_3.jpeg';
+// export * from './cat_4.png';
+// export * from './cat_5.jpg';
